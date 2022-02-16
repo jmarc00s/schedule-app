1beeb43c9322a2df5c9cb5e22330b7ba9b6b1950
João Marcos <jmarcossferreira@hotmail.com>
2022-02-16 12:55:10 -0300

    Melhorando error handling das requisições

--- src/core/hooks/useAxios.ts
+++ src/core/hooks/useAxios.ts
@@ -1,12 +1,31 @@
 import axios, { AxiosRequestConfig } from 'axios';
-import React from 'react';
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useToast } from './useToast';
 
 axios.defaults.baseURL = 'http://localhost:4000';
 
 export function useAxios<T>() {
-  const [data, setData] = React.useState<T | undefined>(undefined);
-  const [loading, setLoading] = React.useState<boolean>(false);
-  const [error, setError] = React.useState<string>('');
+  const [data, setData] = useState<T | undefined>(undefined);
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<string>('');
+  const { showWarningToast } = useToast();
+  const navigate = useNavigate();
+
+  function handleResponseError(status: number) {
+    if (status === 404) {
+      showWarningToast('Não foi possível acessar o recurso no servidor.');
+      return;
+    }
+
+    if (status === 401) {
+      showWarningToast('Sua sessão expirou. Realize o login novamente');
+      navigate('/login');
+      return;
+    }
+
+    showWarningToast('Não foi possível realizar a ação.');
+  }
 
   const request = async (options: AxiosRequestConfig): Promise<T | undefined> => {
     setError('');
@@ -16,8 +35,11 @@ export function useAxios<T>() {
       const { data } = await axios(options);
       setData(data);
       return data;
-    } catch (error) {
-      console.error(error);
+    } catch (error: any) {
+      const { response } = error;
+      if (response) {
+        handleResponseError(response.status);
+      }
     } finally {
       setLoading(false);
     }
