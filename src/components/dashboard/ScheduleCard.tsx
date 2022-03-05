bc188321bb9d90917ded0dfa8d00031c4bd6d77b
João Marcos <jmarcossferreira@hotmail.com>
2022-03-05 10:32:30 -0300

    Adicionando icone atenção na listagem de proximos horários

--- src/components/dashboard/ScheduleCard.tsx
+++ src/components/dashboard/ScheduleCard.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { EStatusSchedule } from 'src/core/enum/status-schedule.enum';
 import { ScheduleModel } from 'src/core/models/schedule.model';
 import Card from '../Card';
+import { IconCalendar } from '../icons/IconCalendar';
+import { IconWarning } from '../icons/IconWarning';
 
 interface ScheduleCardProps {
   schedules: ScheduleModel[];
 }
 
 const ScheduleCard = ({ schedules }: ScheduleCardProps) => {
+  function isPendingSchedule(schedule: ScheduleModel) {
+    return schedule.status === EStatusSchedule.PENDING;
+  }
+
   return (
     <Card title="Próximos horários">
       {!!schedules.length && (
@@ -14,16 +21,22 @@ const ScheduleCard = ({ schedules }: ScheduleCardProps) => {
           {schedules.map((schedule) => (
             <li key={schedule.id} className="p-2 border-b border-gray-200">
               <div className="flex flex-row">
-                <div className="flex flex-col w-2/3">
+                <div className="flex flex-col w-2/4">
                   <p className="text-gray-800 font-medium">{schedule.client?.name}</p>
                   <span className="text-gray-500 text-sm">
                     {schedule.date} {schedule.time}
                   </span>
                 </div>
-                <div className="flex-1 flex items-center">
-                  <div className="px-2 py-1 rounded-full bg-blue-800 text-gray-50 font-semibold text-xs">
+                <div className="flex-1 flex flex-row items-center justify-between">
+                  <span className="px-2 py-1 rounded-full bg-blue-800 text-gray-50 font-semibold text-xs">
                     {schedule.service?.description}
-                  </div>
+                  </span>
+
+                  {isPendingSchedule(schedule) && (
+                    <span className="h-10 w-10 flex items-center justify-center rounded-full bg-yellow-100 text-yellow-600">
+                      {IconWarning}
+                    </span>
+                  )}
                 </div>
               </div>
             </li>
