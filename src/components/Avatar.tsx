ee7de5479ea9768ce060f314a9887f8a8fbb0686
João Marcos <jmarcossferreira@hotmail.com>
2022-03-09 07:30:44 -0300

    Melhorando menu do componente de avatar

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -6,7 +6,7 @@ const Avatar = () => {
   const { user } = useAuth();
 
   return (
-    <div className="flex items-center h-full gap-2 px-3 cursor-pointer hover:bg-indigo-700">
+    <div className="flex items-center h-full gap-2 px-3 cursor-pointer">
       <p>{user?.username}</p>
       <div className="w-8 h-8 rounded-full cursor-pointer">
         <img src="https://cdn-icons-png.flaticon.com/512/149/149071.png" alt="User" />
--- src/components/AvatarMenu.tsx
+++ src/components/AvatarMenu.tsx
@@ -3,32 +3,77 @@ import { Menu } from '@headlessui/react';
 import { IconLogout } from './icons/IconLogout';
 import Avatar from './Avatar';
 import { IconCog } from './icons/IconCog';
+import { useNavigate } from 'react-router-dom';
+import { IconUser } from './icons/IconUser';
+import ConfirmDialog from './ConfirmDialog';
+import useAuth from 'src/core/hooks/useAuth';
 
 const AvatarMenu = () => {
+  const [openConfirmLogoutDialog, setOpenConfirmLogoutDialog] = useState(false);
+  const navigate = useNavigate();
+  const { logout } = useAuth();
+
+  const handleOpenConfirmLogoutDialog = () => setOpenConfirmLogoutDialog(true);
+
+  function handleConfigClick() {}
+
+  function handleProfileClick() {
+    navigate('/user');
+  }
+
+  function handleLogoutClick() {
+    handleOpenConfirmLogoutDialog();
+  }
+
   return (
-    <Menu as="div" className={'relative h-full flex'}>
-      <Menu.Button>
-        <Avatar />
-      </Menu.Button>
-      <Menu.Items className="absolute right-0 z-50 mt-2 text-gray-800 bg-white divide-y divide-gray-100 rounded-md shadow-lg w-42 -bottom-12">
-        <div className="p-2">
-          <Menu.Item>
-            <button className="flex items-center w-full px-4 py-2 text-sm rounded-md hover:bg-gray-100">
-              <span className="text-gray-500">{IconCog(5, 5)}</span>
-              <span className="ml-3">Configurações</span>
-            </button>
-          </Menu.Item>
-        </div>
-        <div className="p-2">
-          <Menu.Item>
-            <button className="flex items-center w-full px-4 py-2 text-sm rounded-md hover:bg-gray-100">
-              <span className="text-gray-500">{IconLogout(5, 5)}</span>
-              <span className="ml-3">Sair</span>
-            </button>
-          </Menu.Item>
-        </div>
-      </Menu.Items>
-    </Menu>
+    <>
+      <Menu as="div" className={'relative inline-block'}>
+        <Menu.Button>
+          <Avatar />
+        </Menu.Button>
+        <Menu.Items className="absolute right-0 z-50 mt-2 text-gray-800 origin-top-right bg-white divide-y divide-gray-100 rounded-md shadow-lg w-42">
+          <div className="p-2">
+            <Menu.Item>
+              <button
+                onClick={handleProfileClick}
+                className="flex items-center w-full px-4 py-2 text-sm rounded-md hover:bg-gray-100"
+              >
+                <span className="text-gray-500">{IconUser(5, 5)}</span>
+                <span className="ml-3">Perfil</span>
+              </button>
+            </Menu.Item>
+          </div>
+          <div className="p-2">
+            <Menu.Item>
+              <button
+                onClick={handleConfigClick}
+                className="flex items-center w-full px-4 py-2 text-sm rounded-md hover:bg-gray-100"
+              >
+                <span className="text-gray-500">{IconCog(5, 5)}</span>
+                <span className="ml-3">Configurações</span>
+              </button>
+            </Menu.Item>
+          </div>
+          <div className="p-2">
+            <Menu.Item>
+              <button
+                onClick={handleLogoutClick}
+                className="flex items-center w-full px-4 py-2 text-sm rounded-md hover:bg-gray-100"
+              >
+                <span className="text-gray-500">{IconLogout(5, 5)}</span>
+                <span className="ml-3">Sair</span>
+              </button>
+            </Menu.Item>
+          </div>
+        </Menu.Items>
+      </Menu>
+      <ConfirmDialog
+        open={openConfirmLogoutDialog}
+        setOpen={setOpenConfirmLogoutDialog}
+        description="Deseja realmente sair da aplicação ?"
+        onConfirm={() => logout()}
+      />
+    </>
   );
 };
 
